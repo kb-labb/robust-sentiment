10074a4d4e5cbc98197572d3cf3638e056c8fd4f
Hillevi Hägglöf <hillevi.hagglof@gmail.com>
2023-04-03 13:08:49 +0200

    Some preprocessing changes

--- preprocessing/twitter-preparation.py
+++ preprocessing/twitter-preparation.py
@@ -5,8 +5,11 @@ import numpy as np
 # Indata is training set and test set. These are merged here. Split into train and test will be perfomed at a later stage in the pipeline.
 # Output is csv file of text,label where 1 = negative, 2 = neutral, 3 = positive.
 
-train_file = "../path/to/train/file.csv"
-test_file = "../path/to/test/file.csv"
+#train_file = "../path/to/file.csv"
+#test_file = "../path/to/test/file.csv"
+
+train_file = "../data/raw/twitter_train_set.csv"
+test_file = "../data/raw/twitter_balanced_test_set.csv"
 
 train_df = pd.read_csv(train_file, delimiter = ",", quotechar = "|", encoding = "utf8")
 test_df = pd.read_csv(test_file, delimiter = ",", quotechar = "|", encoding = "utf8")
@@ -25,4 +28,12 @@ df["label"] = df["label"].replace(["-1", "0", "1"], ["1", "2", "3"])
 df["label"] = pd.to_numeric(df["label"])
 df["label"] = df["label"].astype(int)
 
-df.to_csv("/path/to/out/file.csv")
+
+#df.to_csv("/path/to/out/file.csv")
+df.to_csv("../data/processed/twitter-data.csv")
+
+
+# NOTE TO SELF
+# I also did this in the terminal after execution because I forgot in the actual Python code
+#sed -i -r 's@^\w+,@@g' test.csv
+# To remove first column of id in csv file
